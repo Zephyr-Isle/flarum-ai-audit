4cbe2ab8ee0382758b7408d4ce770f9acfb155f1
Zephyr-Isle <zephyr_isle@outlook.com>
2026-07-29 23:28:34 +0800

    fix: use Promise.resolve() instead of app.booted.then - booted is not thenable

--- js/src/forum/initializers/checkSuicideAlert.ts
+++ js/src/forum/initializers/checkSuicideAlert.ts
@@ -1,9 +1,9 @@
 import app from 'flarum/forum/app';
 import SuicideWarmModal from '../components/SuicideWarmModal';
-
 export default function () {
-  app.booted.then(() => {
+  Promise.resolve().then(() => {
     if (!app.session?.user) return;
+
     app.store.find('notifications').then((notifications: any[]) => {
       const suicideAlerts = notifications.filter((n: any) => {
         const type = n.attribute('type') || n.data?.attributes?.type || n.contentType?.();
