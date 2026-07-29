a3b646094915d1840dec594a99e5becf1fa4854b
Zephyr-Isle <zephyr_isle@outlook.com>
2026-07-29 23:23:17 +0800

    fix: checkSuicideAlert initializer crashes when app.session is undefined

--- js/src/forum/initializers/checkSuicideAlert.ts
+++ js/src/forum/initializers/checkSuicideAlert.ts
@@ -2,9 +2,8 @@ import app from 'flarum/forum/app';
 import SuicideWarmModal from '../components/SuicideWarmModal';
 
 export default function () {
-  if (!app.session.user) return;
-
   app.booted.then(() => {
+    if (!app.session?.user) return;
     app.store.find('notifications').then((notifications: any[]) => {
       const suicideAlerts = notifications.filter((n: any) => {
         const type = n.attribute('type') || n.data?.attributes?.type || n.contentType?.();
